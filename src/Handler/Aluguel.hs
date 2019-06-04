91194e6542389f4581d1a744a85f3735e5498612
Gabriel <gabriel.fumelli@gmail.com>
2019-06-04 19:25:09 +0000

    criando metodo get do form aluguel

--- src/Handler/Aluguel.hs
+++ src/Handler/Aluguel.hs
@@ -16,3 +16,19 @@ formAluguel cid pid = renderBootstrap $ Aluguel
     <*> areq textareaField "Quanto tempo deseja ficar com o produto?" Nothing
     <*> areq textareaField "Defina seu interesse no produto: " Nothing
     <*> areq textareaField "Escreva seu contato: " Nothing
+    
+getAluguelR :: UsuarioId -> ProdutoId -> Handler Html
+getAluguelR cid pid = do 
+    _ <- runDB $ get404 cid
+    _ <- runDB $ get404 pid
+    msg <- getMessage
+    (widget,enctype) <- generateFormPost (formAluguel cid pid)
+    defaultLayout $ do
+        addStylesheet $ StaticR css_bootstrap_css
+        [whamlet|
+            $maybe mensagem <- msg
+                ^{mensagem}
+            <form action=@{AluguelR cid pid} method=post enctype=#{enctype}>
+                ^{widget}
+                <input type="submit" class="w3-button w3-black w3-section" value="Cadastrar">
+        |]
