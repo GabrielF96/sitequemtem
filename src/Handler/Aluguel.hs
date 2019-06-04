f446aa09358be7056844cf143f3088f00da75d2b
Gabriel <gabriel.fumelli@gmail.com>
2019-06-04 19:15:55 +0000

    criando formulario para aluguel

--- src/Handler/Aluguel.hs
+++ src/Handler/Aluguel.hs
@@ -8,3 +8,11 @@ module Handler.Aluguel where
 
 import Import
 import Database.Persist.Postgresql
+
+formAluguel :: UsuarioId -> ProdutoId -> Form Aluguel
+formAluguel cid pid = renderBootstrap $ Aluguel
+    <$> pure pid
+    <*> pure cid
+    <*> areq textareaField "Quanto tempo deseja ficar com o produto?" Nothing
+    <*> areq textareaField "Defina seu interesse no produto: " Nothing
+    <*> areq textareaField "Escreva seu contato: " Nothing
